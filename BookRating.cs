29a827d419733eb6c4cd7dc793ddbbc6e65ca912
hrstmr <37159938+hrstmr@users.noreply.github.com>
2023-03-03 21:00:29 +1100

    Create BookRating.cs

---
+++ BookRating.cs
@@ -0,0 +1,16 @@
+﻿namespace hotchocolate_error_handling;
+
+[UnionType]
+public interface BookRating { }
+
+public class Rating : BookRating
+{
+    public required int Average { get; set; }
+    public int Total { get; set; } = 0;
+}
+
+public class BookUnpublishedError : BookRating
+{
+    public string Message { get; set; } = "Book is not published yet";
+    public required DateTimeOffset ReleaseDate { get; set; }
+}
