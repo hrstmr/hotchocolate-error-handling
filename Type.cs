0d741b5e801ea41ba0df314267d2edc9ba3395f3
hrstmr <37159938+hrstmr@users.noreply.github.com>
2023-03-03 19:49:31 +1100

    Create Type.cs

---
+++ Type.cs
@@ -0,0 +1,12 @@
+﻿namespace hotchocolate_error_handling;
+
+public class Author
+{
+    public required string Name { get; set; }
+}
+
+public class Book
+{
+    public required string Title { get; set; }
+    public required Author Author { get; set; }
+}
