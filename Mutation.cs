407db68a272bdd0c5ed1ac4e360d50a20060a897
hrstmr <37159938+hrstmr@users.noreply.github.com>
2023-03-03 21:52:24 +1100

    Update Mutation.cs

--- Mutation.cs
+++ Mutation.cs
@@ -28,6 +28,39 @@ public class Mutation
             ReleaseDate = DateTimeOffset.UtcNow.AddYears(-1),
         };
     }
+
+    [Error<InSufficientFundException>]
+    [Error<OutOfStockException>]
+    public async Task<Book> BuyBookOrMultiError(string title)
+    {
+        List<Exception> exceptions = new List<Exception>();
+        if (title == "err")
+        {
+            var outOfStock = new OutOfStockException()
+            {
+                SuggestedBook = new()
+                {
+                    Author = new() { Name = "John Doe" },
+                    ReleaseDate = DateTimeOffset.UtcNow,
+                    Title = "Suggested Book"
+                },
+                BackInStockDate = DateTimeOffset.UtcNow.AddDays(5),
+            };
+            exceptions.Add(outOfStock);
+            exceptions.Add(new InSufficientFundException(10, 50));
+        }
+        if (exceptions.Count > 0)
+        {
+            throw new AggregateException(exceptions);
+        }
+
+        return new()
+        {
+            Title = title,
+            Author = new Author { Name = "Jon Skeet" },
+            ReleaseDate = DateTimeOffset.UtcNow.AddYears(-1),
+        };
+    }
 }
 
 public class InSufficientFundException : Exception
@@ -41,4 +74,12 @@ public class InSufficientFundException : Exception
         CurrentBalance = currentBalance;
         RequiredAmount = requiredAmount;
     }
+}
+
+public class OutOfStockException : Exception
+{
+    public Book? SuggestedBook { get; set; }
+    public DateTimeOffset? BackInStockDate { get; set; }
+
+    public OutOfStockException() : base($"Out Of Stock") { }
 }
