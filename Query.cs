188f3e05930633dfc060ebca3c4bcc17eb0d3358
hrstmr <37159938+hrstmr@users.noreply.github.com>
2023-03-03 19:24:21 +1100

    Create Query.cs

---
+++ Query.cs
@@ -0,0 +1,13 @@
+﻿using hotchocolate_error_handling.Types;
+
+namespace hotchocolate_error_handling;
+
+public class Query
+{
+    public Book GetBook() =>
+        new Book
+        {
+            Title = "C# in depth.",
+            Author = new Author { Name = "Jon Skeet" }
+        };
+}
