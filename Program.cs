8343859837700910d189f1bf9ea4de4ea4f11bf9
hrstmr <37159938+hrstmr@users.noreply.github.com>
2023-03-03 19:48:56 +1100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
+using hotchocolate_error_handling;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
+
 var app = builder.Build();
+
+app.MapGraphQL();
 
 app.MapGet("/", () => "Hello World!");
 
