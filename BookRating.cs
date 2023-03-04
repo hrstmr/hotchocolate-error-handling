5694cae7de2a221aa111d0e2d07b6a2cba7ed9cd
hrstmr <37159938+hrstmr@users.noreply.github.com>
2023-03-04 14:06:28 +1100

    Renmaed

--- BookRating.cs
+++ BookRating.cs
@@ -14,3 +14,9 @@ public class BookUnpublishedError : BookRating
     public string Message { get; set; } = "Book is not published yet";
     public required DateTimeOffset ReleaseDate { get; set; }
 }
+
+public class PendingValidationError : BookRating
+{
+    public string Message { get; set; } = "Reviews are being validated by our team";
+    public string? Details { get; set; }
+}
