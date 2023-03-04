49293531c5e8244e8805295bbb85d9cd223cd238
hrstmr <37159938+hrstmr@users.noreply.github.com>
2023-03-04 18:15:01 +1100

    Update Type.cs

--- Type.cs
+++ Type.cs
@@ -1,6 +1,10 @@
 ﻿namespace hotchocolate_error_handling;
 
-public record Book(string Title, Author Author, DateTimeOffset ReleaseDate);
+public record Book(
+  string Title,
+  Author Author,
+  DateTimeOffset ReleaseDate
+);
 
 public record Author(string Name);
 
@@ -8,23 +12,25 @@ public record Author(string Name);
 [ExtendObjectType(typeof(Book))]
 public class BookExtensions
 {
-    // Define a resolver method for a field that returns a BookRating Union
-    public BookRating GetRatingAsUnion([Parent] Book book)
-    {
-        if (book.Title == "Rise of the Sugar Rush")
-            return new PendingValidationError();
+  // Define a resolver method for a field that returns a BookRating Union
+  public BookRating GetRatingAsUnion([Parent] Book book)
+  {
+    if (book.Title == "Rise of the Sugar Rush")
+      return new PendingValidationError();
 
-        return book.ReleaseDate < DateTimeOffset.UtcNow
-            ? new Rating(5)
-            : new BookUnpublishedError(book.ReleaseDate);
-    }
+    return book.ReleaseDate < DateTimeOffset.UtcNow
+      ? new Rating(5)
+      : new BookUnpublishedError(book.ReleaseDate);
+  }
 
-    // As an Error
-    public Rating? GetRatingWithError([Parent] Book book)
-    {
-        if (book.Title == "Rise of the Sugar Rush")
-            return null;
+  // As an Error
+  public Rating? GetRatingWithError([Parent] Book book)
+  {
+    if (book.Title == "Rise of the Sugar Rush")
+      return null;
 
-        return book.ReleaseDate < DateTimeOffset.UtcNow ? new Rating(5) : throw new Exception();
-    }
+    return book.ReleaseDate < DateTimeOffset.UtcNow
+      ? new Rating(5)
+      : throw new Exception();
+  }
 }
